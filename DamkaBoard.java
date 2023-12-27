883640071e3894a648e5efef95848f41a9065256
nathancode26 <154314837+nathancode26@users.noreply.github.com>
2023-12-27 10:38:52 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,7 +5,7 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		//// Put your code here
 		int n = Integer.parseInt(args[0]);
-		for(int i=0 ; i< n  ; i++ ){
+		for(int i=0 ; i< n-1  ; i++ ){
 			int c =n;
 			if(i % 2 == 0){
 				System.out.print("*");
